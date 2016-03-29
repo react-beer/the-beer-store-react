8026c4268b5525e367dc2294e3f5fb31a0129765
brenopolanski <breno.polanski@gmail.com>
2016-03-28 23:21:28 -0300

    #1 - Create file with beers data

---
+++ src/js/utils/data-beers.js
@@ -0,0 +1,98 @@
+module.exports = {
+  beer1: {
+    name: 'Biritis',
+    image: 'img/beers/biritis.png',
+    price: '22,91',
+    status: 'unavailable'
+  },
+  beer2: {
+    name: 'Bamberg O Calibre',
+    image: 'img/beers/bamberg-o-calibre.png',
+    price: '22,93',
+    status: 'available'
+  },
+  beer3: {
+    name: 'Burgman Lager',
+    image: 'img/beers/burgman-lager.png',
+    price: '14,50',
+    status: 'available'
+  },
+  beer4: {
+    name: 'Chimay Rouge',
+    image: 'img/beers/chimay-rouge.png',
+    price: '25,44',
+    status: 'available'
+  },
+  beer5: {
+    name: 'Eisenbahn Pilsen',
+    image: 'img/beers/eisenbahn-pilsen.png',
+    price: '5,25',
+    status: 'available'
+  },
+  beer6: {
+    name: 'Goose Island',
+    image: 'img/beers/goose-island.png',
+    price: '25,90',
+    status: 'unavailable'
+  },
+  beer7: {
+    name: 'Gordelícia',
+    image: 'img/beers/gordelicia.png',
+    price: '17,70',
+    status: 'available'
+  },
+  beer8: {
+    name: 'Hellbeirão Pils',
+    image: 'img/beers/hellbeirao-pils.png',
+    price: '22,99',
+    status: 'available'
+  },
+  beer9: {
+    name: 'Júpiter IPA',
+    image: 'img/beers/jupiter-ipa.png',
+    price: '18,97',
+    status: 'unavailable'
+  },
+  beer10: {
+    name: 'Juan Caloto',
+    image: 'img/beers/juan-caloto.png',
+    price: '21,00',
+    status: 'available'
+  },
+  beer11: {
+    name: 'Leffe Radieuse',
+    image: 'img/beers/leffe-radieuse.png',
+    price: '15,90',
+    status: 'available'
+  },
+  beer12: {
+    name: 'Madalena Stout',
+    image: 'img/beers/madalena-stout.png',
+    price: '21,18',
+    status: 'available'
+  },
+  beer13: {
+    name: 'Cascahell',
+    image: 'img/beers/cascahell.png',
+    price: '22,93',
+    status: 'available'
+  },
+  beer14: {
+    name: 'Schornstein IPA',
+    image: 'img/beers/schornstein-ipa.png',
+    price: '19,90',
+    status: 'available'
+  },
+  beer15: {
+    name: 'Brewdog Punk IPA',
+    image: 'img/beers/brewdog-punk-ipa.png',
+    price: '25,90',
+    status: 'available'
+  },
+  beer16: {
+    name: 'Blue Moon',
+    image: 'img/beers/blue-moon.png',
+    price: '26,23',
+    status: 'unavailable'
+  }
+};
