cba3e76f8107ec6158175658f93a90a45d01f31a
brenopolanski <breno.polanski@gmail.com>
2016-04-02 23:59:54 -0300

    #3 - Add task browser-sync

---
+++ gulp/tasks/browser-sync.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Necessary plugins
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var paths       = require('../paths');
+
+// Call Browser Sync
+module.exports = gulp.task('browser-sync', function() {
+    browserSync({
+        // We need to disable clicks and forms for when we test multiple rooms
+        server: {},
+        middleware: [historyApiFallback()],
+        ghostMode: false
+    });
+});
