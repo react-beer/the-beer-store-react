2843c7ea25e89a204166592231327d905c0f643b
brenopolanski <breno.polanski@gmail.com>
2016-04-05 01:42:16 -0300

    Update 2016-04-05T04:42:01.521Z

--- gulp/tasks/default.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Necessary plugins
-var gulp = require('gulp');
-
-// Call Default
-module.exports = gulp.task('default',
-  ['styles', 'images', 'scripts', 'browser-sync', 'watch']
-);
